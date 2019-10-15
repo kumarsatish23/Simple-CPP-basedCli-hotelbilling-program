a0eca14d7096545f99a8938b07a738dee3caefbc
Satish Kumar Rai <34617630+kumarsatish23@users.noreply.github.com>
2019-10-15 16:10:08 +0530

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,11 +2,16 @@
 #include <conio.h>
 #include <string>
 #include <fstream>
+#include<stdlib.h>
 using namespace std;
-string name, address, email, fbill = "", phone, passw = "password@123", msg = "HOTEL MANAGEMENT SYSTEM \n\n\n", pass;
-int room, hall, menu, total = 0, mainmenu, cnt = 1;
+string name, address, email, fbill = "", passw = "password@123", msg = "HOTEL MANAGEMENT SYSTEM \n\n\n", pass;
+int room, hall, menu, total = 0, mainmenu, adr1, adr2; string phone;
 bool x = true;
-
+class avail
+{
+public:
+	int suit, NAC, AC;
+}a;
 void read()
 {
 	system("cls");
@@ -20,99 +25,132 @@ void read()
 }
 void checkinn()
 {
-	cout << "select Type Of room:\n\n 1-suit 10 available\n 2-non-A.C 3 available\n 3-A.C 6 available\n 4-Mainmenu\n";
+	fstream f;
+	f.open("avail.txt", ios::out | ios::in | ios::binary);
+	f.read((char*)&a, sizeof(a));
+	cout << "select Type Of room:\n\n 1-suit " << a.suit << " available \n 2-non-A.C " << a.NAC << " available \n 3-A.C " << a.AC << " available\n";
 	cin >> room;
 	switch (room)
 	{
-	case 1:
-		total = total + 100;
-		fbill = fbill + "     suit                          100\n";
-		break;
+	case 1:if (a.suit < 1)
+	{
+		cout << "Sorry room not available"; getche(); checkinn();
+	}
+		  total = total + 100;
+		  fbill = fbill + "     suit                          100\n";
+		  a.suit = a.suit - 1; cout << "ROOM BOOKED SUCCESSFULLY\n";
+		  adr1 = f.tellg();
+		  adr2 = sizeof(int);
+		  f.seekp(adr1 - adr2);
+		  f.write((char*)&a.suit, sizeof(a.suit));
+		  break;
 	case 2:
+		if (a.NAC < 1)
+		{
+			cout << "Sorry room not available"; getche(); checkinn();
+		}
 		total = total + 200;
 		fbill = fbill + "     non-A.C                       200\n";
+		a.NAC = a.NAC - 1; cout << "ROOM BOOKED SUCCESSFULLY\n";
+		adr1 = f.tellg();
+		adr2 = sizeof(int);
+		f.seekp(adr1 - 2 * adr2);
+		f.write((char*)&a.NAC, sizeof(a.NAC));
 		break;
 	case 3:
+		if (a.AC < 1)
+		{
+			cout << "Sorry room not available"; getche(); checkinn();
+		}
 		total = total + 300;
 		fbill = fbill + "     A.C                           300\n";
-		break;
-	case 4:
-		fbill = fbill + "     Non                           0\n";
+		a.AC = a.AC - 1; cout << "ROOM BOOKED SUCCESSFULLY\n";
+		adr1 = f.tellg();
+		adr2 = sizeof(int);
+		f.seekp(adr1 - 3 * adr2);
+		f.write((char*)&a.AC, sizeof(a.AC));
 		break;
 	default:
 		cout << "Error! operator is not correct";
-		break;
+		getche(); checkinn();
 	}
+	f.close();
 }
 void functionhall()
 {
-	cout << "select Type Of Function Hall:\n\n 1-Student \n 2-Regular\n 3-Mainmenu\n";
+	cout << "select Type Of Function Hall:\n\n 1-Student \n 2-Regular\n";
 	cin >> hall;
 	switch (hall)
 	{
 	case 1:
 		total = total + 1000;
-		fbill = fbill + "     function hall student         1000\n";
+		fbill = fbill + "     function hall student         1000\n"; cout << "FUNCTION HALL BOOKED\n";
 		break;
 	case 2:
 		total = total + 2000;
-		fbill = fbill + "     function hall Regular         2000\n";
-		break;
-	case 3:
-		fbill = fbill + "     Non                           0\n";
+		fbill = fbill + "     function hall Regular         2000\n"; cout << "FUNCTION HALL BOOKED\n";
 		break;
 	default:
 		cout << "Error! option is not correct";
-		break;
+		getche(); functionhall();
 	}
 }
 void resturent()
 {
-	
-		cout << "Restaurant:\n\n =============Menu=============" << endl
-			<< "1. Press '1' to Buy Pizza" << endl
-			<< "2. Press '2' to Buy Rice" << endl
-			<< "3. Press '3' to Buy Spaghetti" << endl
-			<< "4. Press '4' to Buy Tea" << endl
-			<< "5. Press '5' to Buy Garlic bread" << endl
-			<< endl << "Please select Items from the menu: ";
-		cin >> menu;
-		switch (menu)
-		{
-		case 1:
-			total = total + 100;
-			fbill = fbill + "     Pizza                         100\n";
-			break;
-		case 2:
-			total = total + 60;
-			fbill = fbill + "     Rice                          60\n";
-			break;
-		case 3:
-			total = total + 80;
-			fbill = fbill + "     Spaghetti                     80\n";
-			break;
-		case 4:
-			total = total + 25;
-			fbill = fbill + "     Tea                           25\n";
-			break;
-		case 5:
-			total = total + 70;
-			fbill = fbill + "     Garlic bread                  70\n";
-			break;
-		default:
-			cout << "Error! operator is not correct";
-			break;
+
+	cout << "Restaurant:\n\n =============Menu=============" << endl
+		<< "1. Press '1' to Buy Pizza" << endl
+		<< "2. Press '2' to Buy Rice" << endl
+		<< "3. Press '3' to Buy Spaghetti" << endl
+		<< "4. Press '4' to Buy Tea" << endl
+		<< "5. Press '5' to Buy Garlic bread" << endl
+		<< endl << "Please select Items from the menu: ";
+	cin >> menu;
+	switch (menu)
+	{
+	case 1:
+		total = total + 100;
+		fbill = fbill + "     Pizza                         100\n";
+		break;
+	case 2:
+		total = total + 60;
+		fbill = fbill + "     Rice                          60\n";
+		break;
+	case 3:
+		total = total + 80;
+		fbill = fbill + "     Spaghetti                     80\n";
+		break;
+	case 4:
+		total = total + 25;
+		fbill = fbill + "     Tea                           25\n";
+		break;
+	case 5:
+		total = total + 70;
+		fbill = fbill + "     Garlic bread                  70\n";
+		break;
+	default:
+		cout << "Error! operator is not correct";
+		break;
 
 	}
 }
 int main()
 {
+	fstream f;
+	f.open("avail.txt", ios::out | ios::in | ios::binary);
+	a.suit = 10; a.NAC = 10; a.AC = 10; f.write((char*)&a, sizeof(a)); f.close();
 	while (1)
 	{
 		system("cls");
-		cout << msg + "Enter password$: ";
-		cin >> pass;
-		if (pass != passw)
+		cout << msg + "Enter password$: "; int i = 0; char z;
+
+		char ch;
+		ch = _getch();
+		while (ch != 13) {
+			pass.push_back(ch);
+			cout << '*';
+			ch = _getch();
+		}		if (pass != passw)
 		{
 			system("cls");
 
@@ -125,13 +163,14 @@ int main()
 			int big;
 			cout << msg + "1.NEWCLIENT" << endl
 				<< "2.CHECK RECORD" << endl
+
 				<< "ENTER OPTION : ";
 			cin >> big;
 			if (big == 2)
 			{
 				read();
 			}
-			if (big == 1)
+			else if (big == 1)
 			{
 				system("cls");
 
@@ -150,24 +189,21 @@ int main()
 				{
 					cout << msg + "1.CHECKINN" << endl
 						<< "2.FUNCTION HALL" << endl
-						<< "3.RESTURENT"<<endl
-						<< "ENTER OPTION : ";					
+						<< "3.RESTURENT" << endl
+						<< "ENTER OPTION : ";
 					cin >> mainmenu;
 					switch (mainmenu)
 					{
 					case 1:
-						fbill = fbill + to_string(cnt);
-						cnt++;
+
 						checkinn();
 						break;
 					case 2:
-						fbill = fbill + to_string(cnt);
-						cnt++;
+
 						functionhall();
 						break;
 					case 3:
-						fbill = fbill + to_string(cnt);
-						cnt++;
+
 						resturent();
 						break;
 					default:
@@ -175,12 +211,21 @@ int main()
 						break;
 					}
 					string end;
-					cout << "Do you want to continue (y/n): ";
+					cout << "Do you want to book anything else (y/n)?: ";
 					cin >> end;
-					if (end == "n")
+					if (end == "y")
+					{
+						y = true;
+					}
+					else if (end == "n")
 					{
 						y = false;
 					}
+					else
+					{
+						cout << "Invalid"; exit(0);
+					}
+
 				}
 				system("cls");
 				cout << msg + "==========final bill==========\n\n"
@@ -197,7 +242,7 @@ int main()
 					<< "            Grand Total    " << total + total * 0.16 << endl
 					<< "==============================\n"
 					;
-				std::fstream fs;
+				fstream fs;
 				fs.open("record.txt", std::fstream::in | std::fstream::out | std::fstream::app);
 				fs
 					<< "Name of Client           :" << name << endl
@@ -213,6 +258,10 @@ int main()
 				fbill = "";
 				total = 0;
 				system("\n pause");
+			}
+			else
+			{
+				cout << "Invalid"; getch(); main();
 			}
 		}
 	}
